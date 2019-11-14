83a854dbecc490888a0ad79b00c11c041e64080d
Connor Dawson <ccdawson@ucsc.edu>
2019-11-14 14:34:28 -0800

    changed upload file back

--- upload_file.php
+++ upload_file.php
@@ -17,18 +17,18 @@ if ($_FILES["file"]["size"] < 20480000)
 		
 		// 判断当期目录下的 upload 目录是否存在该文件
 		// 如果没有 upload 目录，你需要创建它，upload 目录权限为 777
-		if (file_exists("upload/" . $_FILES["file"]["name"]))
+		if (file_exists("upload/" $_FILES["file"]["name"]))
 		{
 			echo $_FILES["file"]["name"] . " file already exist ";
 		}
 		else
 		{
 			// 如果 upload 目录不存在该文件则将文件上传到 upload 目录下
-			$targetdir = '/CSE11a';
+			$targetdir = '/CSE11a/';
 
-			$targetfile = $targetdir/basename( $_FILES['file']['name']);
+			$targetfile = $targetdir . basename( $_FILES['file']['name']);
 
-			move_uploaded_file($_FILES['file']['/tmp'], $targetfile);
+			move_uploaded_file($_FILES['file']['tmp'], $targetfile);
 			echo "save as: " . "upload/" . $_FILES["file"]["name"];
 
 			header("Refresh:5;url=/up/upload/");
